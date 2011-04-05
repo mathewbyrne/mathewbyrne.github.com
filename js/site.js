9bd1b9c83c43e496b7f6e32f6f1d36db8e092f72
Mathew Byrne <mathewbyrne@gmail.com>
2011-04-06 03:33:34 +1000

    Added basic navigation interactions.

--- js/site.js
+++ js/site.js
@@ -4,6 +4,29 @@ $(function () {
 	var bc = $('#business-card');
 	bc.css('margin-top', bc.outerHeight() / -2);
 	bc.hide().fadeIn('slow');
+	
+	var tl = $('#tag-line'),
+	    tl_text = tl.text(),
+	    tl_timeout;
+	
+	$('#navigation a').mouseover(function () {
+        var self = $(this);
+        if (tl_timeout) {
+            window.clearTimeout(tl_timeout);
+            tl_timeout = null;
+        }
+        tl.animate({opacity: 0.01}, function () {
+            tl.text(self.attr('title')).animate({opacity: 1});
+        });
+    });
+	
+	$('#navigation').mouseout(function (e) {
+        tl_timeout = window.setTimeout(function () {
+            tl.animate({opacity: 0.01}, function () {
+                tl.text(tl_text).animate({opacity: 1});
+            });
+        });
+	});
 });
     
 })(jQuery);
