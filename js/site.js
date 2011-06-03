971ee937973aea75fc45b5c7ac5cf9b6792ee19a
Mathew Byrne <mathewbyrne@gmail.com>
2011-06-04 03:44:36 +1000

    Updates to CSS to separate home/pages.

--- js/site.js
+++ js/site.js
@@ -7,12 +7,12 @@ $(function () {
 	bc.hide().fadeIn('slow');
 	
 	// Footer slide up transition.
-	var footer = $('#footer');
-	var footer_margin_top = $('#footer').css('margin-top');
-	footer.css({'margin-top': 0, 'opacity': 0.01}).animate({
-	        'margin-top': footer_margin_top,
-	        'opacity': 1
-	    }, 'slow');
+	//var footer = $('#footer');
+	//var footer_margin_top = $('#footer').css('margin-top');
+	//footer.css({'margin-top': 0, 'opacity': 0.01}).animate({
+	//        'margin-top': footer_margin_top,
+	//        'opacity': 1
+	//    }, 'slow');
 });
     
 })(jQuery);
