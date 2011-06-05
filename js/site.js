4c45b433966bb3874c3a4a40a7d3e6dfcdf12c9d
Mathew Byrne <mathewbyrne@gmail.com>
2011-06-05 15:58:42 +1000

    Added Javascript current page indicator.

--- js/site.js
+++ js/site.js
@@ -5,6 +5,14 @@ $(function () {
 	var bc = $('#business-card');
 	bc.css('margin-top', bc.outerHeight() / -2);
 	bc.hide().fadeIn('slow');
+	
+	// Highlight the current page in navigation.
+	var path = window.location.pathname;
+	$('#navigation a').each(function () {
+		if (path.indexOf($(this).attr('href')) === 0) {
+			$(this).addClass('current');
+		}
+	});
 });
     
 })(jQuery);
